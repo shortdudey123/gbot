8f444b3c8b3cd563cc78273318110f55762e8f1b
shortdudey123 <shortdudey123@gmail.com>
2014-07-09 22:04:03 -0700

    change name

--- gbot.py
+++ gbot.py
@@ -15,5 +15,5 @@ import src.bot as bot
 
 
 if __name__ == "__main__":
-    gbot = bot.IRCBot(server="chat.freenode.com", nick="gbot", port=6667, realName='gbot', identify='', debug=True, connectDelay=4)
+    gbot = bot.IRCBot(server="chat.freenode.com", nick="grbot", port=6667, realName='gbot', identify='', debug=True, connectDelay=4)
     gbot.run()
