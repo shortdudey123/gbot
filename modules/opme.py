996e0a685d5cffaa0a276d16c4b81dac47d7b9a1
shortdudey123 <shortdudey123@gmail.com>
2014-07-12 03:24:22 -0700

    add opme module

---
+++ modules/opme.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# =============================================================================
+# file = opme.py
+# description = gbot module
+# author = GR <https://github.com/shortdudey123>
+# create_date = 2014-07-12
+# mod_date = 2014-07-12
+# version = 0.1
+# usage = loaded by gbot
+# notes =
+# python_ver = 2.7.6
+# =============================================================================
+
+adminOnly = True
+
+def execModule(channel, message, nick, bot):
+	retCommands = []
+	return retCommands
+
+if __name__ == "__main__":
+    filename = __file__.split('.')[0]
+    help(filename)
