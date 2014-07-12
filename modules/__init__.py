0fd3916edf06c8efc422d2d9e1b177f2880c31b8
shortdudey123 <shortdudey123@gmail.com>
2014-07-12 04:00:23 -0700

    fix mass import

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,4 +3,5 @@ for module in os.listdir(os.path.dirname(__file__)):
     if module == '__init__.py' or module[-3:] != '.py' or module[-4:] != '.pyc' or module[-4:] != '.pyo':
         continue
     __import__(module[:-3], locals(), globals())
+del module
 del os
