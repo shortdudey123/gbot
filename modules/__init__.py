767d2cfb4913ebcd892618e9ae0e79ecce6c5b5f
shortdudey123 <shortdudey123@gmail.com>
2014-07-12 18:23:17 -0700

    add reference

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,5 @@
 # based on info from http://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python
+# more info in my own question: http://stackoverflow.com/questions/24718759/how-do-i-dynamically-reload-a-module-in-a-custom-package
 import os
 for module in os.listdir(os.path.dirname(__file__)):
     if module[0:8] != '__init__' and module[-3:] == '.py':
