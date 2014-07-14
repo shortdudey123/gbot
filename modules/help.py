ee83dc88aeeb9700000dfd7a4fc12d56a1d615e7
shortdudey123 <shortdudey123@gmail.com>
2014-07-14 04:00:32 -0700

    add note line

--- modules/help.py
+++ modules/help.py
@@ -29,6 +29,7 @@ def execModule(channel, message, nick, botSelf):
         botSelf.bot.sendMessage(channel, 'I am an instance of gbot (https://github.com/shortdudey123/gbot)')
         botSelf.bot.sendMessage(channel, 'Commands: {0}'.format(commands))
         botSelf.bot.sendMessage(channel, 'For help on the commands: help <command>')
+        botSelf.bot.sendMessage(channel, 'Note: all commands must be preceded by {0}:'.format(botSelf.bot.nick))
     elif len(message.split()) == 2 and message.split()[1] in botSelf.loadedModules.keys():
         for line in botSelf.loadedModules[message.split()[1]]['help'].split('\n'):
             botSelf.bot.sendMessage(channel, line)
