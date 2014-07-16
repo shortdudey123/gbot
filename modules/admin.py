c7f70b73bcbef9695229ee4be111817866b37bbd
shortdudey123 <shortdudey123@gmail.com>
2014-07-16 01:08:07 -0700

    fix indices

--- modules/admin.py
+++ modules/admin.py
@@ -27,11 +27,11 @@ def execModule(channel, message, nick, botSelf):
     if len(message.split()) == 2 and message.split()[1].lower() == 'list':
         admins = ','.join(botSelf.admins)
         botSelf.bot.sendMessage(channel, 'Admins: {0}'.format(admins))
-    elif len(message.split()) == 4:
-        adminNick = message.split()[3]
-        if message.split()[2].lower() == 'add':
+    elif len(message.split()) == 3:
+        adminNick = message.split()[2]
+        if message.split()[1].lower() == 'add':
             botSelf.addAdmin(adminNick)
-        if message.split()[2].lower() == 'del':
+        if message.split()[1].lower() == 'del':
             try:
                 delMsg = botSelf.deleteAdmin(adminNick)
                 botSelf.bot.sendMessage(channel, '{0}'.format(delMsg))
